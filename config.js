7323e0febed82b2430b28d7447c80e0ff01633cf
Ahmed Osman <ahmedosmanazrk@gmail.com>
2017-04-16 17:13:39 +0200

    adding API token for aqicn

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  database: process.env.MONGO_URI || 'localhost/moonshot'
+  database: process.env.MONGO_URI || 'localhost/moonshot',
+  apiToken: 'f595957addd1d4ff3abf7e788486b58b5bfa276a'
 };
